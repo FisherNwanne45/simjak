d225a60875586f8aa8799710eb283d1d08b6969e
FisherNwanne45 <fisherfresh@outlook.com>
2026-02-14 20:04:15 +0100

    resolved admin session

--- resources/adminsession.php
+++ resources/adminsession.php
@@ -9,21 +9,20 @@ if (!$connection) {
 }
 
 // Selecting Database
-$db = mysqli_select_db($connection, "auromisr_cr");
+//$db = mysqli_select_db($connection, "auromisr_cr");
 
-session_start();// Starting Session
+session_start(); // Starting Session
 // Storing Session
-$user_check=$_SESSION['login_user'];
+$user_check = $_SESSION['login_user'];
 // SQL Query To Fetch Complete Information Of User
-$ses_sql=mysqli_query($connection,"select * from login where username='$user_check'");
+$ses_sql = mysqli_query($connection, "select * from login where username='$user_check'");
 $row = mysqli_fetch_assoc($ses_sql);
-$login_session =$row['username'];
+$login_session = $row['username'];
 
-$id = $row['id']; 
-  
+$id = $row['id'];
 
-if(!isset($login_session)){
-mysqli_close($connection); // Closing Connection
-header('Location: login.php'); // Redirecting To Home Page
+
+if (!isset($login_session)) {
+    mysqli_close($connection); // Closing Connection
+    header('Location: login.php'); // Redirecting To Home Page
 }
-?>  
